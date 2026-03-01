cc78e787c6c98261ca45de7c589b718de90019dc
Ven0m0 <ven0m0.wastaken@gmail.com>
2026-03-01 01:46:58 +0100

    Delete patch_test.py

--- patch_test.py
+++
@@ -1,5 +0,0 @@
-"""
-This module previously contained an ad-hoc patch helper script that rewrote
-src/validation.rs for a one-off change. The automation has been removed to
-avoid committing temporary patch helpers to the repository.
-"""
